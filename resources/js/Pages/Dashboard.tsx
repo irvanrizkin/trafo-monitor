c16286295270df29aadf34d4bdf9f17a5fd8b54e
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2024-08-06 16:45:07 +0700

    feat(dashboard/v1): added appbar

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import {DashboardPropsV1} from '@/types';
-import {Box, Button, Container, Grid, Typography} from "@mui/material";
+import {AppBar, Box, Button, Container, Grid, Toolbar, Typography} from "@mui/material";
 import TrafoCard from "@/Components/Trafo/TrafoCard";
 import GoogleMapReact from "google-map-react";
 
@@ -32,50 +32,59 @@ export default function Dashboard({ trafos }: DashboardPropsV1) {
     }
 
     return (
-        <Container maxWidth={"md"}>
-            <Box sx={{ my: 4 }}>
-                <Typography variant="h4" gutterBottom>
-                    Daftar Trafo V1
-                </Typography>
-                <Box
-                    sx={{
-                        display: 'flex',
-                        justifyContent: 'flex-end',
-                        marginBottom: 2,
-                    }}
-                >
-                    <Button sx={{ mx: 2 }} href={route('trafo.create')} variant="contained">Add Trafo</Button>
-                    <Button href={route('v2.dashboard')} variant="contained">Go to Version 2</Button>
-                </Box>
-                <Box
-                    sx={{
-                        width: '100%',
-                        height: '400px',
-                        display: 'flex',
-                    }}
-                    component="map"
-                >
-                    <GoogleMapReact
-                        bootstrapURLKeys={{ key: mapApiKey }}
-                        defaultCenter={defaultProps.center}
-                        defaultZoom={defaultProps.zoom}
-                        yesIWantToUseGoogleMapApiInternals
-                        onGoogleApiLoaded={({ map, maps }) => renderMarker(map, maps)}
+        <>
+            <AppBar>
+                <Toolbar>
+                    <Typography variant="h6" noWrap component="div">
+                        Trafo Monitoring
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Container maxWidth={"xl"}>
+                <Box sx={{ my: 4 }}>
+                    <Typography variant="h4" gutterBottom>
+                        Daftar Trafo V1
+                    </Typography>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'flex-end',
+                            marginBottom: 2,
+                        }}
                     >
+                        <Button sx={{ mx: 2 }} href={route('trafo.create')} variant="contained">Add Trafo</Button>
+                        <Button href={route('v2.dashboard')} variant="contained">Go to Version 2</Button>
+                    </Box>
+                    <Box
+                        sx={{
+                            width: '100%',
+                            height: '400px',
+                            display: 'flex',
+                        }}
+                        component="map"
+                    >
+                        <GoogleMapReact
+                            bootstrapURLKeys={{ key: mapApiKey }}
+                            defaultCenter={defaultProps.center}
+                            defaultZoom={defaultProps.zoom}
+                            yesIWantToUseGoogleMapApiInternals
+                            onGoogleApiLoaded={({ map, maps }) => renderMarker(map, maps)}
+                        >
 
-                    </GoogleMapReact>
+                        </GoogleMapReact>
+                    </Box>
+                    <Grid container spacing={2} mt={1.5}>
+                        {trafos.map((trafo) => (
+                            <Grid item key={trafo.id} xs={12} sm={6} md={4} xl={3}>
+                                <TrafoCard
+                                    trafo={trafo}
+                                    version={1}
+                                />
+                            </Grid>
+                        ))}
+                    </Grid>
                 </Box>
-                <Grid container spacing={2} mt={1.5}>
-                    {trafos.map((trafo) => (
-                        <Grid item key={trafo.id} xs={12} sm={6} md={4}>
-                            <TrafoCard
-                                trafo={trafo}
-                                version={1}
-                            />
-                        </Grid>
-                    ))}
-                </Grid>
-            </Box>
-        </Container>
+            </Container>
+        </>
     );
 }
