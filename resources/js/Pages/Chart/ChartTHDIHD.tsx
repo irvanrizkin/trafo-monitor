316f3da305aa42c4c22de634198d68d19d01ab2f
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2024-09-14 07:38:31 +0700

    chart thd aggregation table

--- resources/js/Pages/Chart/ChartTHDIHD.tsx
+++ resources/js/Pages/Chart/ChartTHDIHD.tsx
@@ -9,6 +9,8 @@ import ShowAssignmentIcon from "@mui/icons-material/Assignment";
 import ButtonEndHref from "@/Components/Shared/ButtonEndHref";
 import {rstBarChart, rstBarChartString, rstLineChart} from "@/helpers/generator/chart-generator";
 import timeMinuteString from "@/helpers/converter/date-time";
+import calculateMetrics from "@/helpers/analysis/calculate-metric";
+import AggregationRST from "@/Components/Chart/AggregationRST";
 
 export default function ChartTHDIHD({
                                         trafo,
@@ -25,12 +27,22 @@ export default function ChartTHDIHD({
         tData: thdVoltages.map(voltage => voltage.voltage_t),
     });
 
+    const voltageRAggregation = calculateMetrics(thdVoltages.map(voltage => voltage.voltage_r));
+    const voltageSAggregation = calculateMetrics(thdVoltages.map(voltage => voltage.voltage_s));
+    const voltageTAggregation = calculateMetrics(thdVoltages.map(voltage => voltage.voltage_t));
+    const { voltage_r = 0, voltage_s = 0, voltage_t = 0 } = thdVoltages[thdVoltages.length - 1] || {};
+
     const metricCurrents = rstBarChartString({
         labels: thdCurrents.map(current => timeMinuteString(new Date(current.created_at))),
         rData: thdCurrents.map(current => current.current_r),
         sData: thdCurrents.map(current => current.current_s),
         tData: thdCurrents.map(current => current.current_t),
     });
+
+    const currentRAggregation = calculateMetrics(thdCurrents.map(current => current.current_r));
+    const currentSAggregation = calculateMetrics(thdCurrents.map(current => current.current_s));
+    const currentTAggregation = calculateMetrics(thdCurrents.map(current => current.current_t));
+    const { current_r = 0, current_s = 0, current_t = 0 } = thdCurrents[thdCurrents.length - 1] || {};
 
     const renderMarker = (map: any, maps: any) => {
         return new maps.Marker({
@@ -76,11 +88,22 @@ export default function ChartTHDIHD({
                         >
                             <Typography variant={"h6"}>Total Harmonics Distortion Voltage (THDv)</Typography>
                             <Bar data={metricVoltages}/>
-                            <Paper sx={{ p: 2 }}>
-                                <Typography>VR : {Math.round((0 + Number.EPSILON) * 100) / 100}</Typography>
-                                <Typography>VS : {Math.round((0 + Number.EPSILON) * 100) / 100}</Typography>
-                                <Typography>VT : {Math.round((0 + Number.EPSILON) * 100) / 100}</Typography>
-                            </Paper>
+                            <Container sx={{ p: 2 }}>
+                                <AggregationRST
+                                    rMax={voltageRAggregation.max}
+                                    sMax={voltageSAggregation.max}
+                                    tMax={voltageTAggregation.max}
+                                    rMin={voltageRAggregation.min}
+                                    sMin={voltageSAggregation.min}
+                                    tMin={voltageTAggregation.min}
+                                    rAvg={voltageRAggregation.avg}
+                                    sAvg={voltageSAggregation.avg}
+                                    tAvg={voltageTAggregation.avg}
+                                    rLatest={voltage_r}
+                                    sLatest={voltage_s}
+                                    tLatest={voltage_t}
+                                />
+                            </Container>
                         </Box>
                     </Grid>
                     <Grid item xs={12} md={4}>
@@ -93,11 +116,22 @@ export default function ChartTHDIHD({
                         >
                             <Typography variant={"h6"}>Total Harmonics Distortion Current (THDi)</Typography>
                             <Bar data={metricCurrents}/>
-                            <Paper sx={{ p: 2 }}>
-                                <Typography>IR : {Math.round((0 + Number.EPSILON) * 100) / 100}</Typography>
-                                <Typography>IS : {Math.round((0 + Number.EPSILON) * 100) / 100}</Typography>
-                                <Typography>IT : {Math.round((0 + Number.EPSILON) * 100) / 100}</Typography>
-                            </Paper>
+                            <Container sx={{ p: 2 }}>
+                                <AggregationRST
+                                    rMax={currentRAggregation.max}
+                                    sMax={currentSAggregation.max}
+                                    tMax={currentTAggregation.max}
+                                    rMin={currentRAggregation.min}
+                                    sMin={currentSAggregation.min}
+                                    tMin={currentTAggregation.min}
+                                    rAvg={currentRAggregation.avg}
+                                    sAvg={currentSAggregation.avg}
+                                    tAvg={currentTAggregation.avg}
+                                    rLatest={current_r}
+                                    sLatest={current_s}
+                                    tLatest={current_t}
+                                />
+                            </Container>
                         </Box>
                     </Grid>
                     <Grid item xs={12} md={4}>
