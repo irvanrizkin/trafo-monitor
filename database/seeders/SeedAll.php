28cf3638df4069c7ee516c399b48bc404f69a487
Irvan Rizki Nugraha <irvanriskinugraha@90@gmail.com>
2025-02-08 17:17:26 +0700

    fix: fix seed all function

--- database/seeders/SeedAll.php
+++ database/seeders/SeedAll.php
@@ -1,5 +1,22 @@
 <?php
 
+use Database\Seeders\ActivePowerSeeder;
+use Database\Seeders\AmbientTemperatureSeeder;
+use Database\Seeders\ApparentPowerSeeder;
+use Database\Seeders\CurrentSeeder;
+use Database\Seeders\FrequencySeeder;
+use Database\Seeders\GisSeeder;
+use Database\Seeders\KFactorSeeder;
+use Database\Seeders\MaxValueSeeder;
+use Database\Seeders\OilLevelSeeder;
+use Database\Seeders\PowerFactorSeeder;
+use Database\Seeders\PowerLossSeeder;
+use Database\Seeders\PressureSeeder;
+use Database\Seeders\ReactivePowerSeeder;
+use Database\Seeders\TemperatureSeeder;
+use Database\Seeders\THDVoltageSeeder;
+use Database\Seeders\VoltageDropSeeder;
+use Database\Seeders\VoltageSeeder;
 use Illuminate\Database\Seeder;
 
 class SeedAll extends Seeder
@@ -7,13 +24,23 @@ class SeedAll extends Seeder
     public function run()
     {
         $this->call([
+            ActivePowerSeeder::class,
+            AmbientTemperatureSeeder::class,
+            ApparentPowerSeeder::class,
+            CurrentSeeder::class,
+            FrequencySeeder::class,
             GisSeeder::class,
+            KFactorSeeder::class,
+            MaxValueSeeder::class,
+            OilLevelSeeder::class,
             PowerFactorSeeder::class,
             PowerLossSeeder::class,
             PressureSeeder::class,
+            ReactivePowerSeeder::class,
             TemperatureSeeder::class,
             THDVoltageSeeder::class,
             VoltageDropSeeder::class,
+            VoltageSeeder::class,
         ]);
     }
 }
