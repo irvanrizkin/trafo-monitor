6811e8f1b34ab64a940de9830fb72863011c48dd
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2025-06-01 20:00:58 +0700

    feat: remove other chart than k-factor

--- app/Http/Controllers/Chart/ChartPKAController.php
+++ app/Http/Controllers/Chart/ChartPKAController.php
@@ -5,9 +5,7 @@ namespace App\Http\Controllers\Chart;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Helpers\Aggregator;
 use App\Models\KFactor;
-use App\Models\PowerLoss;
 use App\Models\Trafo;
-use App\Models\TriplenCurrent;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -18,45 +16,27 @@ class ChartPKAController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $trafoId = $request->route('trafoid');
+        $trafoId = $request->route("trafoid");
 
         $trafo = Trafo::find($trafoId);
         if (!$trafo) {
-            return redirect()->route('not-found');
+            return redirect()->route("not-found");
         }
 
         $aggregator = new Aggregator();
 
-        $powerLosses = PowerLoss::where('trafo_id', $trafoId)
-            ->orderBy('created_at', 'desc')
-            ->limit(12)
-            ->get();
-        $powerLosses = $powerLosses->reverse()->values();
-
-        $kFactors = KFactor::where('trafo_id', $trafoId)
-            ->orderBy('created_at', 'desc')
+        $kFactors = KFactor::where("trafo_id", $trafoId)
+            ->orderBy("created_at", "desc")
             ->limit(12)
             ->get();
         $kFactors = $kFactors->reverse()->values();
 
-        $triplenCurrents = TriplenCurrent::where('trafo_id', $trafoId)
-            ->orderBy('created_at', 'desc')
-            ->limit(12)
-            ->get();
-        $triplenCurrents = $triplenCurrents->reverse()->values();
+        $kFactorMetrics = $aggregator->aggregate($kFactors, "k_factor");
 
-        $powerLossMetrics = $aggregator->aggregate($powerLosses, 'power_loss');
-        $kFactorMetrics = $aggregator->aggregate($kFactors, 'k_factor');
-        $triplenCurrentMetrics = $aggregator->aggregate($triplenCurrents, 'triplen_current');
-
-        return Inertia::render('Chart/ChartPKA', [
-            'trafo' => $trafo,
-            'powerLosses' => $powerLosses,
-            'kFactors' => $kFactors,
-            'triplenCurrents' => $triplenCurrents,
-            'powerLossMetrics' => $powerLossMetrics,
-            'kFactorMetrics' => $kFactorMetrics,
-            'triplenCurrentMetrics' => $triplenCurrentMetrics,
+        return Inertia::render("Chart/ChartPKA", [
+            "trafo" => $trafo,
+            "kFactors" => $kFactors,
+            "kFactorMetrics" => $kFactorMetrics,
         ]);
     }
 }
